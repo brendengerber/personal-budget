d7e730a3b9b9af65d0497a563bf0a4ab83337d0f
Brenden Gerber <>
2022-12-21 21:25:35 -0600

    Add starter files and basic logic for server and apiRouter

---
+++ api-router.js
@@ -0,0 +1,14 @@
+//Imports necessary modules
+const express = require('express');
+
+//Creates the router
+const apiRouter = express.Router();
+
+
+
+
+
+
+
+//Exports the router
+module.exports = apiRouter;
---
+++ server.js
@@ -0,0 +1,38 @@
+//*********Copy this to the google doc? is it better ordering than that one? */
+
+//Imports necessary modules
+const express = require('express');
+const morgan = require('morgan');
+const helmet = require('helmet');
+
+const app = express();
+
+//Remove after development to minimize unnecessary realtime logs on server
+app.use(morgan('tiny'));
+
+//Security Middleware
+app.use(helmet());
+app.disable('x-powered-by');
+
+//Parses request bodies to json
+app.use(express.json());
+
+//Mounts the api router
+const apiRouter = require('./api-router.js');
+app.use('/api', apiRouter);
+ 
+//Sets and serves the static folder, remove html extension from url
+app.use(express.static(`${__dirname}/public`, {extensions: ['html']}));
+ 
+//Catch all to send all requests that are not defined by http methods to index.html
+app.get('/*', (req, res, next) => {
+    res.sendFile(`${__dirname}/public/index.html`);
+  });  
+
+//Sets the port and starts the server
+const PORT = process.env.PORT || 3000
+
+app.listen(PORT, () => {
+  console.log(`Server is listening on port ${PORT}`);
+});
+  
