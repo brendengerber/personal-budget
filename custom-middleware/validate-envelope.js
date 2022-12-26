e331ad7f000283970ede81c54fb05782a003c604
Brenden Gerber <>
2022-12-26 13:25:29 -0600

    Add validate-envelope.js and logic

---
+++ custom-middleware/validate-envelope.js
@@ -0,0 +1,14 @@
+const validateEnvelope = (req, res, next) => {
+    try{
+        if(req.body.budget && req.body.category){
+            next();
+        }else{
+            res.status(400).send({message: "Incorrect envelope format"})
+        }
+    } catch (err){
+       res.status(500).send(err) 
+    }
+};
+
+
+module.exports = validateEnvelope;
