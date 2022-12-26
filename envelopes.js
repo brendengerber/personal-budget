8acd79d7fc7b10dacdaec1dfd40768b4b3bd7a3d
Brenden Gerber <>
2022-12-26 17:02:59 -0600

    Add description to envelopes.js

--- envelopes.js
+++ envelopes.js
@@ -1,4 +1,12 @@
 //Variable used to hold envelopes, will replace with database
+/* 
+Envelopes should be objects with the following format:
+{
+    category: string,
+    budget: number,
+    id: number
+}
+*/
 
 let envelopes = []
 
