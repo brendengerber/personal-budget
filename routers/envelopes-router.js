f6615960d05cc4240f0889c2dbc91e2ac72e6638
Brenden Gerber <>
2022-12-26 16:11:49 -0600

    Add logic for post to envelopes-router.js

--- routers/envelopes-router.js
+++ routers/envelopes-router.js
@@ -1,11 +1,20 @@
 //Imports necessary modules
 const express = require('express');
+const envelopes = require('../envelopes.js');
+const validateEnvelope = require('../custom-middleware/validate-envelope.js');
+const assignEnvelopeId = require('../custom-middleware/assign-envelope-Id.js');
+const addEnvelope = require('../custom-middleware/add-envelope.js');
 
 //Creates the router
 const envelopesRouter = express.Router();
 
+envelopesRouter.get('/', (req, res, next) => {
+    res.send(envelopes);
+});
 
-
+envelopesRouter.post('/', validateEnvelope, assignEnvelopeId, addEnvelope, (req, res, next) => {
+    res.send(req.body);
+});
 
 //Exports the router
 module.exports = envelopesRouter;
