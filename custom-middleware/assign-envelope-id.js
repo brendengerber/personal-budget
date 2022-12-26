01693b9a02ce868fa86580109f4d6533e0a07bff
Brenden Gerber <>
2022-12-26 15:18:52 -0600

    Add logic for if envelopes array is empty

--- custom-middleware/assign-envelope-id.js
+++ custom-middleware/assign-envelope-id.js
@@ -1,14 +1,19 @@
 //Assigns an id to the envelope in the req body based on the current highest id
 
-const envelopes = require('../envelopes.js')
+const envelopes = require('../envelopes.js');
 
 const assignEnvelopeId = (req, res, next) => {
     try{
-        const newId = envelopes[envelopes.length - 1].id + 1
-        req.body.id = newId
-        next()
+        let newId;
+        if(envelopes.length === 0){
+            newId = 1;
+        }else{
+            newId = envelopes[envelopes.length - 1].id + 1;
+            req.body.id = newId;
+        }
+        next();
     }catch(err){
-        res.status(500).send(err)
+        res.status(500).send(err);
     }
 }
 
