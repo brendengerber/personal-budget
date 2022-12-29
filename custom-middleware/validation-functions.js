79f387ae28e1503dfb41625d4812b7876a1314cc
Brenden Gerber <>
2022-12-29 11:15:40 -0600

    Rename validate-envelope.js to validation-functions.js

---
+++ custom-middleware/validation-functions.js
@@ -0,0 +1,30 @@
+//Validates the envelope format for post requsts
+const validateEnvelope = (req, res, next) => {
+    try{
+        if(typeof(req.body.budget) === 'number' && typeof(req.body.category) === 'string'){
+            next();
+        }else{
+            res.status(400).send({message: 'Incorrect envelope format'});
+        }
+    } catch(err){
+       res.status(500).send(err);
+    }
+};
+
+//Validates an ID ensuring it is a number
+const validateId = (req, res, next) => {
+    try{
+        if(typeof(Number(req.params.id)) === 'number'){
+            req.id === req.params.id
+            next()
+        }
+    }catch(err){
+        res.status(500).send(err)
+    }
+}
+
+module.exports = {
+    validateEnvelope,
+    validateId
+};
+
