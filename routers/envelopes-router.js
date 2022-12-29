0ffec636257d325b272db2cd5d60e5ac846fc2d3
Brenden Gerber <>
2022-12-29 11:19:23 -0600

    Update imports of envelopes-router.js

--- routers/envelopes-router.js
+++ routers/envelopes-router.js
@@ -1,12 +1,14 @@
 //Imports necessary modules
 const express = require('express');
 const envelopes = require('../envelopes.js');
-const validateEnvelope = require('../custom-middleware/validate-envelope.js');
+const { validateEnvelope, validateId} = require('../custom-middleware/validation-functions.js')
 const assignEnvelopeId = require('../custom-middleware/assign-envelope-Id.js');
 const addEnvelope = require('../custom-middleware/add-envelope.js');
 
 //Creates the router
 const envelopesRouter = express.Router();
+
+//Add param here to validate and attatch id first. Should this also find and attatch the envelope in question? Mayhaps (that function is in database functions now)
 
 envelopesRouter.get('/', (req, res, next) => {
     res.send(envelopes);
@@ -15,6 +17,10 @@ envelopesRouter.get('/', (req, res, next) => {
 envelopesRouter.post('/', validateEnvelope, assignEnvelopeId, addEnvelope, (req, res, next) => {
     res.send(req.body);
 });
+
+envelopesRouter.delete('/:id', (req, res, next) => {
+ //Add logic here
+})
 
 //Exports the router
 module.exports = envelopesRouter;
