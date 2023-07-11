86a6a2dec57bd89c1c9bfa0982f9c56748805b4f
Brenden Gerber <10082638+brendengerber@users.noreply.github.com>
2023-07-11 14:41:42 -0500

    Add transactions table

--- envelopes.sql
+++ envelopes.sql
@@ -3,6 +3,11 @@ CREATE EXTENSION IF NOT EXISTS "uuid-ossp";
 CREATE TABLE "envelopes" (
   "id" uuid DEFAULT gen_random_uuid() PRIMARY KEY,
   "category" varchar(50),
-  "budget" numeric(100, 2)
+  "budget" numeric(100, 2),
 );
 
+CREATE TABLE "transactions" (
+  "id" uuid DEFAULT gen_random_uuid() PRIMARY KEY,
+  "envelope_id" uuid REFERENCES envelopes(id),
+  "amount" numeric (100, 2)
+);
